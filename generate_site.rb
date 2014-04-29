fe659574a8944e227542e6e1fe65c3db4fa0f706
Maria <mariai@capital.bg>
2014-04-29 20:25:59 +0300

    generate_site, single template

--- generate_site.rb
+++ generate_site.rb
@@ -1,37 +1,16 @@
 #!/bin/env ruby
 # encoding: utf-8
-header_template='<!DOCTYPE html>
+template = '<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Spellbound\'s world</title>
-		<link rel="stylesheet" type="text/css" href="%{styles_path}">
+		<link rel="stylesheet" type="text/css" href="../styles/site_styles.css">
 	</head>
 	<body>
-		<h1>Spellbound\'s world</h1>' 
-header_home=header_template	% {styles_path: 'styles/site_styles.css'}
-header_bg=header_template % {styles_path: '../styles/site_styles.css'}
-header_en=header_template % {styles_path: '../styles/site_styles.css'}
-welcome='<div class="welcome">
-			<ul>
-				<em>
-					<li>...</li>
-					<li>На подложка от жълто и огнено,</li>
-					<li>всеки лист е вълшебна врата</li>
-					<li>към пътеки до днес необхождани.</li>
-					<li>Нося в джоба си ключ към света.</li>
-				</em>
-			</ul>
-		</div>'
-motto_template='<div class="motto">
-			<em>%{motto}</em>
-		</div>' 
-motto_bg=motto_template % {motto: '"Ако имах собствен свят, всичко би било безсмислица.<br/>
-				Нищо не би било каквото е, защото всичко би било каквото не е..."'}
-motto_en=motto_template % {motto: '"If I had a world of my own, everything would be nonsense.<br/>
-				   Nothing would be what it is, because everything would be what it isn\'t..."'}
-social_template='<div class="social">
-			<h3>%{title}</h3>
+		<h1>Spellbound\'s world</h1>
+		<div class="social">
+			<h3>%{social}</h3>
 			<ul>
 				<li>
 					<img width="15px" src="../images/FaceBook-icon.png">
@@ -54,11 +33,9 @@ social_template='<div class="social">
 					</a>
 				</li>
 			</ul>
-		</div>'
-social_bg=social_template % {title: 'Чародейства...', facebook: 'Книгата с лицата', twitter: 'Чуруликанки', youtube: 'Музикалната кутия', duolingo: 'Зелени сови'}
-social_en=social_template % {title: 'Charms...', facebook: 'Book of faces', twitter: 'Chirping about', youtube: 'Music box', duolingo: 'Green owls'}
-work_template='<div class="work">
-			<h3>%{title}</h3>
+		</div>
+		<div class="work">
+			<h3>%{work}</h3>
 			<ul>
 				<li>
 					<img width="15px" src="../images/apple-touch-icon.png">
@@ -71,29 +48,39 @@ work_template='<div class="work">
 					</a>
 				</li>
 			</ul>
-		</div>'
-work_bg=work_template % {title: '...и заклинания', capital: 'Мария в Кръга', mail: 'Писма в бутилка'}
-work_en=work_template % {title: '...and incantations', capital: 'Maria in the Circle', mail: 'Letters in a bottle'}
-links='<div class="links">
-			<a href="bg/test.html"><img src="images/bg-icon.png"></a>
-			<a href="en/test.html"><img src="images/gb-icon.png"></a>
 		</div>
-	</body>
-</html>'
-links_bg='<div class="links">
-			<a href="../test.html"><img width="15px" src="../images/home_icon.png"></a>
-			<a href="test.html"><img src="../images/bg-icon.png"></a>
+		<div class="links">
+			<a href="../bg/test.html"><img src="../images/bg-icon.png"></a>
 			<a href="../en/test.html"><img src="../images/gb-icon.png"></a>
 		</div>
 	</body>
 </html>'
-links_en='<div class="links">
-			<a href="../test.html"><img width="15px" src="../images/home_icon.png"></a>
-			<a href="../bg/test.html"><img src="../images/bg-icon.png"></a>
-			<a href="test.html"><img src="../images/gb-icon.png"></a>
-		</div>
-	</body>
-</html>'
-File.write "C:/Users/User/Documents/GitHub/Spellbound13.github.io/test.html", header_home + welcome + links
-File.write "C:/Users/User/Documents/GitHub/Spellbound13.github.io/bg/test.html", header_bg + motto_bg + social_bg + work_bg + links_bg
-File.write "C:/Users/User/Documents/GitHub/Spellbound13.github.io/en/test.html", header_en + motto_en + social_en + work_en + links_en
+
+bg_text = {
+	title: '"Ако имах собствен свят, всичко би било безсмислица.<br/>
+		Нищо не би било каквото е, защото всичко би било каквото не е..."',
+	social: 'Чародейства...',
+	facebook: 'Книгата с лицата',
+	twitter: 'Чуруликанки',
+	youtube: 'Музикалната кутия',
+	duolingo: 'Зелени сови',
+	work: '...и заклинания',
+	capital: 'Мария в Кръга',
+	mail: 'Писма в бутилка',
+}
+
+en_text = {
+	title: '"If I had a world of my own, everything would be nonsense.<br/>
+	   Nothing would be what it is, because everything would be what it isn\'t..."',
+	social: 'Charms...',
+	facebook: 'Book of faces',
+	twitter: 'Chirping about',
+	youtube: 'Music box',
+	duolingo: 'Green owls',
+	work: '...and incantations',
+	capital: 'Maria in the Circle',
+	mail: 'Letters in a bottle',
+}
+
+File.write "./bg/test.html", template%bg_text
+File.write "./en/test.html", template%en_text
