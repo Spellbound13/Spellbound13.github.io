879db051e7032f1fa8e87ad354ffa8a16954426e
Maria <mariai@capital.bg>
2014-05-28 21:02:27 +0300

    greeter & VAT calculator, plus overall bill

--- greeter.rb
+++ greeter.rb
@@ -5,6 +5,7 @@ name = gets.chomp
 puts "Hello #{name}!"
 
 products_list = []
+sum = 0
 
 print "Enter number of articles "
 number = gets.chomp.to_i
@@ -15,14 +16,24 @@ number = gets.chomp.to_i
 	product[:article] = gets.chomp
 	print "Enter price "
 	product[:price] = gets.chomp.to_i
+	print "Enter ammount "
+	product[:ammount] = gets.chomp.to_i
 	products_list << product
 end 
 
 products_list.each do |product|
 	tax = product[:price]*20/100
 	end_price = product[:price]+tax
+	total_price = product[:ammount] * end_price
+	sum += total_price  # <= shorter way of typing "sum = sum + total_price"
 	puts ""
 	puts "article: " + product[:article]
+	puts "price: " + product[:price].to_s
 	puts "tax: #{tax}"
 	puts "end price: #{end_price}"
+	puts "ammount: "  + product[:ammount].to_s
+	puts "total price: #{total_price}"
 end
+
+puts ""
+puts "overall bill is: #{sum}"
