eacc430aba6d7aa53b74b88008ae34fbbb8c2d2e
Maria <mariai@capital.bg>
2014-06-25 21:14:36 +0300

    RPS game choices check

--- RPS.rb
+++ RPS.rb
@@ -1,8 +1,17 @@
 print "Hello, players!\n"
 print "Player 1, make your choice!\n"
 choice_1 = gets.chomp.downcase
+while (choice_1 != "rock") and (choice_1 != "paper") and (choice_1 != "scissors")
+	puts "Invalid choice!"
+	choice_1 = gets.chomp.downcase
+end
+
 print "Player 2, make your choice!\n"
 choice_2 = gets.chomp.downcase
+while (choice_2 != "rock") and (choice_2 != "paper") and (choice_2 != "scissors")
+	puts "Invalid choice!"
+	choice_2 = gets.chomp.downcase
+end
 
 player1_wins = {"rock"=>"scissors", "scissors"=>"paper", "paper"=>"rock",}
 
---
+++ loop.rb
@@ -0,0 +1,23 @@
+#i = 0 
+#loop do
+#	puts "Hello"
+#	i = i + 1
+#	if i == 5
+#		break
+#	end
+#end
+
+
+#i = 0
+#while i < 5
+#	puts "Hello"
+#	i = i + 1
+#end
+
+
+
+input = gets.chomp
+while input != ""
+	puts "Hello #{input}" 
+	input = gets.chomp
+end	
